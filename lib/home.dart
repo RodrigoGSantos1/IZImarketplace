00f7dfd5ad640c7ed8ea8bcf929d7eae20405cf7
Rodrigo Guedes <rodrigo.guantos@gmail.com>
2020-10-30 13:21:16 -0300

    Textfild

--- lib/home.dart
+++ lib/home.dart
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
                   ),),
 
                   TextField(
-                    
+
                   ),
                   TextField(
                   keyboardType: TextInputType.emailAddress,
